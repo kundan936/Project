33ab3a49ad8a6721f057b5a0d69872ba8cd302ba
kundan936 <96304394+kundan936@users.noreply.github.com>
2024-03-21 12:10:06 +0530

    Update Halwa.java

--- Halwa.java
+++ Halwa.java
@@ -4,7 +4,7 @@ public abstract class Halwa extends Sweet{
 
 	public Halwa()
 	{
-		System.out.println("is a type of Halwa. carrot halwa");
+		System.out.println("is a type of Halwa.");
 	}
 	public abstract void makeHalwa();
 }
